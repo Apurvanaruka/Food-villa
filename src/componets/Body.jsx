f9183f3f0c791f505e0d0464e4b64715e2c142eb
Apurvanaruka <apurvanaruka1@gmail.com>
2023-11-27 22:14:30 +0530

    search functionality works

--- src/componets/Body.jsx
+++ src/componets/Body.jsx
@@ -1,39 +1,42 @@
 import RestaurantsCard from "./RestautrentCard";
-import { restaurants } from "../contants";
+import { restaurantsList, imageUrl } from "../contants";
 import { useState } from "react";
 
-
+function FilterData(searchText,restaurants){
+    const data = restaurants.filter(
+        (restaurant) => {
+            return restaurant.info.name.includes(searchText);
+        });
+        return data;      
+};
 
 const Body = () => {
-    let searchTxt = "search";
 
-    const [searchText, setSearchInput] = useState(); // return variable and function to update the variable
-    const [restaurantsCard, setrestaurantsCard] = useState(restaurants);
+    const [searchText, setSearchInput] = useState(""); // return variable and function to update the variable
+    const [restaurantsCard, setrestaurantsCard] = useState(restaurantsList);
 
     return (
         <>
             <div
                 // use React variable for state variables 
                 className='search-bar'
-                key={1}
-            >
+                key={1}>
                 <input type="text"
                     placeholder="search"
                     value={searchText}
                     onChange={(e) => { setSearchInput(e.target.value) }}
                 />
                 <button onClick={() => {
-
+                    const data = FilterData(searchText,restaurantsList);
+                    setrestaurantsCard(data);
                 }}>Search</button>
             </div>
 
 
-            <div className='restaurants-list'
-                key={2}
-            >
+            <div className='restaurants-list' key={2}>
                 {
-                    restaurantsCard.restaurants.map((restaurant) => {
-                        return <RestaurantsCard {...restaurant} />
+                    restaurantsCard.map((restaurant) => {
+                        return <RestaurantsCard {...restaurant.info} key={restaurant.info.id}/>
                     })
                 }
             </div>
--- src/componets/RestautrentCard.jsx
+++ src/componets/RestautrentCard.jsx
@@ -1,13 +1,14 @@
-
-const RestaurantsCard = ({ image_link, name, address, rating, keys }) => {
+const imageUrl = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+const RestaurantsCard = ({ cloudinaryImageId, name, areaName, avgRating, cuisines ,costForTwo}) => {
+    // console.log(id);
     return (
-        <div className='restaurants-card'
-            key={keys}
-        >
-            <img src={image_link} alt={name} />
+        <div className='restaurants-card'>
+            <img src={imageUrl+cloudinaryImageId} alt={name} ></img>
             <h2>{name}</h2>
-            <h4>{address}</h4>
-            <h3>{rating} star</h3>
+            <h4>{areaName}</h4>
+            <h6>{avgRating} star</h6>
+            <h3>{cuisines.join(", ")}</h3>
+            <h4>{costForTwo}</h4>
         </div>
     );
 }
