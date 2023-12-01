43ba23a55abcf9b48814790ae2dbdebee5a1aca6
Apurvanaruka <apurvanaruka1@gmail.com>
2023-12-01 20:39:58 +0530

    Add simmer and NotFound page

---
+++ src/componets/Notfound.jsx
@@ -0,0 +1,4 @@
+const Notfound = () => (
+    <h1>Item Not Found!</h1>
+)
+export default Notfound;
---
+++ src/componets/Simmer.jsx
@@ -0,0 +1,23 @@
+const RestaurantsCard = () => {
+    return (
+        <div className='simmer-card'>
+            <img ></img>
+            <h3>________</h3>
+            <h4>________</h4>
+            <h6>____</h6>
+            <h4>___________</h4>
+            <h5>_____</h5>
+        </div>
+    )
+}
+
+const Simmer = () => {
+    return (
+        <div className="restaurants-list">
+            {Array(20).fill("").map(() => <RestaurantsCard />)}
+        
+        </div>
+    );
+}
+
+export default Simmer;
