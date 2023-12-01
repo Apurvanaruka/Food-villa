bda9b9d52bbac4e90ccf4106119d9b77c315581b
Apurvanaruka <apurvanaruka1@gmail.com>
2023-12-01 20:40:23 +0530

    ep6 homework

--- src/componets/Body.jsx
+++ src/componets/Body.jsx
@@ -1,37 +1,36 @@
 import RestaurantsCard from "./RestautrentCard";
-import { restaurantsList } from "../contants";
 import { useState, useEffect } from "react";
+import Simmer from "./Simmer";
+import Notfound from "./Notfound";
 
-function FilterData(searchText,restaurants){
+function FilterData(searchText, restaurants) {
     const data = restaurants.filter(
         (restaurant) => {
-            return restaurant.info.name.includes(searchText);
+            return restaurant.info.name.toLowerCase().includes(searchText.toLowerCase());
         });
-        return data;      
+    return data;
 };
 
 
 const Body = () => {
-
-    const [searchText, setSearchInput] = useState(""); // return variable and function to update the variable
-    const [ allrestaurants, setAllRestaurants ] = useState(restaurantsList);
-    const [ filteredRestaurants, setFilteredRestuarants ] = useState(allrestaurants);
-
-
+    const [searchText, setSearchInput] = useState('');
+    const [allrestaurants, setAllRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestuarants] = useState(allrestaurants);
     useEffect(() => {
         getResaurants();
-    },[]);
+    }, []);
 
-    async function getResaurants(){
+    async function getResaurants() {
         const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.5922846&lng=76.62683059999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
         const json = await data.json();
-        console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]?.info);
         setAllRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setFilteredRestuarants(allrestaurants)
-    } 
-    
+        setFilteredRestuarants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
 
-    return (
+    // (condition) ? true : false
+    // console.log(allrestaurants.length);
+
+    return (allrestaurants?.length === 0) ? <Simmer /> : (
         <>
             <div
                 // use React variable for state variables 
@@ -43,17 +42,16 @@ const Body = () => {
                     onChange={(e) => { setSearchInput(e.target.value) }}
                 />
                 <button onClick={() => {
-                    const data = FilterData(searchText,allrestaurants);
+                    const data = FilterData(searchText, allrestaurants);
                     setFilteredRestuarants(data);
-                    console.log(data);
                 }}>Search</button>
             </div>
 
-
             <div className='restaurants-list' key={2}>
                 {
+                    (filteredRestaurants?.length === 0) ? <Notfound /> :   
                     filteredRestaurants.map((restaurant) => {
-                        return <RestaurantsCard {...restaurant.info} key={restaurant.info.id}/>
+                        return <RestaurantsCard {...restaurant.info} key={restaurant.info.id} />
                     })
                 }
             </div>
